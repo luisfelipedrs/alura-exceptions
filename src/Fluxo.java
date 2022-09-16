43fcf494fb0eaf8c95d85b45f5aa607b8dd5be28
Luis Felipe <luisfelipe@MACBOOK-PRO-DE-ADESKTOP.local>
2022-09-16 16:59:10 -0300

    criacao dos blocos try catch

--- src/Fluxo.java
+++ src/Fluxo.java
@@ -2,7 +2,14 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        try {
+            metodo1();
+        }
+        catch (ArithmeticException | NullPointerException ex) {
+            ex.printStackTrace();
+            String msg = ex.getMessage();
+            System.out.println("Exception: " + msg);
+        }
         System.out.println("Fim do main");
     }
 
@@ -14,14 +21,11 @@ public class Fluxo {
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-        for (int i = 0; i <= 5; i++) {
+        for (int i = 1; i <= 5; i++) {
             System.out.println(i);
-            try {
-                int a = i / 0;
-            }
-            catch (ArithmeticException ex) {
-                System.out.println("ArithmeticException");
-            }
+            int a = i / 0;
+            //Conta cc = null;
+            //cc.deposita();
         }
         System.out.println("Fim do metodo2");
     }
